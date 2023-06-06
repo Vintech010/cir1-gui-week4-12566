9dde2915f9c73a11647da65372b974d04878d8c1
Vintech010 <azakisakuya@gmail.com>
2023-06-06 13:30:56 +0700

    first intential

---
+++ bin/dart_list.dart
@@ -0,0 +1,6 @@
+void main(){
+  List<String> vnl = ["Thailand","japan","Korean"];
+  for (int index = 0; index < vnl.length;index++){
+    print(vnl[index]);
+  }
+}
---
+++ test/dart_week4_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_week4_dart/dart_week4_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
